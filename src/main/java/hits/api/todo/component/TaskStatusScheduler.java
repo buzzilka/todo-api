f538891fde551763ea5786d29d570551bab7641a
Vladislav Karpuk <karpuk-05@mail.ru>
2025-04-21 04:39:52 +0700

    #8.1 Updating task statuses at each server startup

--- src/main/java/hits/api/todo/component/TaskStatusScheduler.java
+++ src/main/java/hits/api/todo/component/TaskStatusScheduler.java
@@ -3,6 +3,7 @@ package hits.api.todo.component;
 import hits.api.todo.entity.TaskEntity;
 import hits.api.todo.enums.TaskStatus;
 import hits.api.todo.repository.TaskRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,20 +19,19 @@ public class TaskStatusScheduler {
 
     private final TaskRepository repository;
 
+    @PostConstruct
+    public void updateTaskStatusesOnStartup(){
+        updateTaskStatuses();
+    }
+
     @Scheduled(cron = "0 0 0 * * *")
     public void updateTaskStatuses() {
         List<TaskEntity> tasks = repository.findAll();
         Date now = new Date();
 
         for (TaskEntity task : tasks) {
-            if (task.getStatus() == TaskStatus.COMPLETED || task.getStatus() == TaskStatus.LATE) {
-                if (now.after(task.getDeadline())) {
-                    task.setStatus(TaskStatus.OVERDUE);
-                } else {
-                    task.setStatus(TaskStatus.ACTIVE);
-                }
-            } else {
-                task.setStatus(TaskStatus.ACTIVE);
+            if (task.getStatus() == TaskStatus.ACTIVE && task.getDeadline() != null && task.getDeadline().before(now)) {
+                task.setStatus(TaskStatus.OVERDUE);
             }
         }
 
