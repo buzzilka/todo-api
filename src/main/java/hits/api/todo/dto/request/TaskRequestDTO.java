5f92233674c5e366394baac0779f6d701beff53c
Vladislav Karpuk <karpuk-05@mail.ru>
2025-04-21 19:03:55 +0700

    #FIX Title and description length limits

--- src/main/java/hits/api/todo/dto/request/TaskRequestDTO.java
+++ src/main/java/hits/api/todo/dto/request/TaskRequestDTO.java
@@ -10,9 +10,10 @@ import java.util.Date;
 @Data
 public class TaskRequestDTO {
     @NotNull
-    @Size(min = 4, message = "Title length cannot be less than 4 characters")
+    @Size(min = 4, max = 255, message = "Title length cannot be less than 4 and more than 255 characters")
     private String title;
 
+    @Size(max = 255, message = "Description length cannot be more than 255 characters")
     private String description;
 
     private Date deadline;
